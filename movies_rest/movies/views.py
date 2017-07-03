9afca503e32acda153d1cb9c020f7c35774b36ef
Maciej Barelkowski <maciejbarel@gmail.com>
2017-07-03 16:07:14 +0200

    Add all movies view

--- movies_rest/movies/views.py
+++ movies_rest/movies/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView, Response
+
+from .serializers import MovieSerializer
+from .models import Movie
 
 # Create your views here.
+
+
+class MoviesView(APIView):
+
+    def get(self, request):
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, context={"request": request})
+        return Response(serializer.data)
