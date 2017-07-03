9c5dd246c60b9fbe4e4e169d5e1c60431e8d5583
Maciej Barelkowski <maciejbarel@gmail.com>
2017-07-03 16:32:38 +0200

    Add single movie view (get, put and delete methods)

--- movies_rest/movies/views.py
+++ movies_rest/movies/views.py
@@ -1,4 +1,6 @@
+from django.http import Http404
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.views import APIView, Response
 
 from .serializers import MovieSerializer
@@ -11,5 +13,36 @@ class MoviesView(APIView):
 
     def get(self, request):
         movies = Movie.objects.all()
-        serializer = MovieSerializer(movies, context={"request": request})
+        serializer = MovieSerializer(movies, many=True, context={"request": request})
         return Response(serializer.data)
+
+
+class MovieView(APIView):
+
+    def get_object(self, id):
+        try:
+            movie = Movie.objects.get(id=id)
+            return movie
+        except Movie.DoesNotExist:
+            raise Http404
+
+    def get(self, request, id):
+        movie = self.get_object(id=id)
+        serializer = MovieSerializer(movie, context={"request": request})
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        movie = self.get_object(id=id)
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        movie = self.get_object(id=id)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def post(self, request, id):
+        pass
